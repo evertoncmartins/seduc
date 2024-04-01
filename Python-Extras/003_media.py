1d2460319823208795d972593f30179016771004
Everton Martins <ecmdigital@hotmail.com>
2024-04-01 13:58:15 -0300

    Add comentário.

--- Python-Extras/003_media.py
+++ Python-Extras/003_media.py
@@ -1,3 +1,5 @@
+# Desenvolva um programa em Python que solicite ao usuário que insira as notas de três provas, calcule a média dessas notas e, em seguida, exiba o resultado da média calculada.
+
 n1 = float(input("Insira a primeira nota: "))
 n2 = float(input("Insira a primeira nota: "))
 n3 = float(input("Insira a primeira nota: "))
