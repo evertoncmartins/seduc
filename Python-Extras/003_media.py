6cc3f1a99088047676094054ead3311051df3f9f
Everton Martins <ecmdigital@hotmail.com>
2024-04-01 13:55:45 -0300

    Criação do exercício média.

---
+++ Python-Extras/003_media.py
@@ -0,0 +1,7 @@
+n1 = float(input("Insira a primeira nota: "))
+n2 = float(input("Insira a primeira nota: "))
+n3 = float(input("Insira a primeira nota: "))
+
+media = (n1 + n2 + n3) / 3
+
+print("Média: {}".format(media))
