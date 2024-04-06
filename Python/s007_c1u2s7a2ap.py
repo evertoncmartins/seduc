ec3a91b147eb894a1e6aa798850657b62b140f8d
Everton Martins <ecmdigital@hotmail.com>
2024-04-05 21:22:39 -0300

    Ajustando comentário

--- Python/s007_c1u2s7a2ap.py
+++ Python/s007_c1u2s7a2ap.py
@@ -2,7 +2,6 @@
 Faça um programa em Python que solicite a quantidade de um produto desejado pelo cliente e verifique se há disponibilidade em estoque. Considere que o estoque mínimo é 10 unidades. 
 
 Portanto, se a quantidade desejada for maior ou igual a 10, exiba a mensagem: "Produto disponível em estoque. Aproveite!". Caso contrário, mostre a mensagem: "Produto indisponível no momento. Volte mais tarde!".
-
 """
 qtde = int(input("Digite a qtde do produto em estoque: "))
 
