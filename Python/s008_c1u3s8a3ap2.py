716b68dc6e73a2bcf63063eb06d9334b842b7d7e
Everton Martins <ecmdigital@hotmail.com>
2024-04-14 16:05:39 -0300

    Add exercício

---
+++ Python/s008_c1u3s8a3ap2.py
@@ -0,0 +1,17 @@
+"""
+Uma empresa tem uma vaga de emprego que requer:
+- Pelo menos 3 anos de experiência em Python E
+- Pelo menos 2 anos de experiência em machine learning OU
+- Um diploma de mestrado em ciência da computação.
+Escreva um programa que determine se um candidato é elegível para a vaga.
+"""
+
+exp_python = int(input("Tempo de experiência com Python: "))
+exp_ml = int(input("Tempo de experiência com Machine Learning: "))
+diploma = input(
+    "Tem diplima de mestrado em Ciência da Computação? (S/N): ").lower()
+
+if exp_python >= 3 and exp_ml >= 2 or diploma == 's':
+    print("É elegível para a vaga.")
+else:
+    print("Não é elegível para a vaga.")
