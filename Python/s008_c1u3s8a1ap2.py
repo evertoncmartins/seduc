e99f4596df809342dcca163ee09055c8c5457a71
Everton Martins <ecmdigital@hotmail.com>
2024-04-14 16:57:27 -0300

    Alteração no código

--- Python/s008_c1u3s8a1ap2.py
+++ Python/s008_c1u3s8a1ap2.py
@@ -5,11 +5,22 @@ Um site de vendas on-line oferece descontos em livros com base nas seguintes reg
 Escreva um programa que aplique essas regras e calcule o preço final do livro.
 """
 
+# Solicita informações sobre o livro
 best = input("O livro é best-seller? (S/N): ").lower()
 lancamento = int(input("Quanto tempo o livro foi lançado?: "))
 qtde_compra = int(input("Quantos livros está comprando?: "))
+valor = float(input("Digite o valor total da compra: "))
 
-if best == 's' or lancamento > 2 and qtde_compra > 3:
-    print("Desconto de 25%")
-else:
-    print("Desconto de 20%")
+# Calcula o desconto baseado nas regras
+desconto = 0
+
+if best == 's' or lancamento > 2:
+    desconto += 0.20
+if qtde_compra >= 3:
+     desconto += 0.05
+
+ # Calcula o preço final com desconto
+preco_final = valor - (valor * desconto)   
+
+# Exibe o preço final
+print("Total: {:.2f}".format(preco_final))
