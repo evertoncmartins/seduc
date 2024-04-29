e2c15ee927875c9a8caa0269bceec7637fa02886
Everton Martins <ecmdigital@hotmail.com>
2024-04-29 10:59:35 -0300

    Add exercise

--- Processos/s010_ex3.py
+++ Processos/s010_ex3.py
@@ -1,11 +1,14 @@
 class Termometro:
-    def __init__(self, temperatura=0):
-        self.__temperatura_celsius = temperatura
+    def __init__(self, temperatura_celsius):
+        self.__temperatura_celsius = temperatura_celsius
+
+    def set_temperatura_celsius(self, temperatura_celsius):
+        self.__temperatura_celsius = temperatura_celsius
 
     def get_temperatura_fahrenheit(self):
         return (self.__temperatura_celsius * 9/5) + 32
 
+
 # Uso da classe
-termometro = Termometro()
-termometro.set_temperatura_fahrenheit(68)
-print(termometro.get_temperatura_fahrenheit())  # Deve mostrar 68
+termometro = Termometro(20)  # Defina a temperatura inicial em Celsius
+print(termometro.get_temperatura_fahrenheit())
