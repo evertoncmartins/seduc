47e0e43ca2011dbd1580063b1e67565a85369ce6
Everton Martins <ecmdigital@hotmail.com>
2024-05-09 11:13:05 -0300

    Add exercise

---
+++ Lógica/Python/s012_ex1.py
@@ -0,0 +1,23 @@
+# Você está programando um app de entrega de alimentos. O app deve calcular o custo de entrega com base nas seguintes condições:
+
+# Se o pedido é inferior a R$10, há uma taxa de entrega fixa de R$5;
+# Se o pedido é de R$10 ou mais, mas menos de R$20, a taxa de entrega é de R$3;
+# Se o pedido é de R$20 ou mais, a entrega é gratuita.
+# Se usuário é membro do programa de fidelidade do app, ele tem desconto de R$2 em qualquer taxa de entrega aplicável.
+
+# Escreva o algoritmo de um programa que pegue o valor do pedido e a informação de que o usuário é ou não membro do programa de fidelidade e, então, calcule e imprima a taxa de entrega final.
+
+pedido = float(input('Valor do pedido: '))
+membro = input('Membro (S/N): ').lower()
+
+if pedido < 10:
+    pedido += 5
+elif pedido < 20:
+    pedido += 3
+else:
+    pedido += 0
+
+if membro == 's':
+    pedido -= 2
+
+print(f'Valor total: {pedido}')
