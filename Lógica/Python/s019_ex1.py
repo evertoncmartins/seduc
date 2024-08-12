21efaa40f60d7718477f53417bbd2df524dcbf86
Everton Martins <ecmdigital@hotmail.com>
2024-08-12 10:49:09 -0300

    att

--- Lógica/Python/s019_ex1.py
+++ Lógica/Python/s019_ex1.py
@@ -4,6 +4,6 @@ N = int(input('Digite um valor para N: '))
 # Itera sobre o intervalo de 1 a N
 for i in range(1, N + 1):
     # Imprime a primeira linha
-    print(f"{i} {i * i} {i * i * i}")
+    print(f'{i} {i * i} {i * i * i}')
     # Imprime a segunda linha
-    print(f"{i} {i * i + 1} {i * i * i + 1}")
+    print(f'{i} {i * i + 1} {i * i * i + 1}')
