b83518b0e5e72c01d4f97c668eb8b9f078d3cf0d
Everton Martins <ecmdigital@hotmail.com>
2024-08-19 08:26:22 -0300

    add exemplo2 s20

---
+++ Lógica/Python/s020_ex2.py
@@ -0,0 +1,11 @@
+# Exemplo 2
+
+# Simulando do...while em Python
+
+while True:
+    num = int(input('Digite um número inteiro positivo ou negativo (para sair): '))
+    print(f'Número digitado: {num}')
+
+    if num < 0 :
+        print('Fim')
+        break
