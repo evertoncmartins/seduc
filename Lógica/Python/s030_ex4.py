ba3634086a6c584c77b3630828d19d6fe36b5e48
Everton Martins <ecmdigital@hotmail.com>
2024-11-25 15:45:33 -0300

    att

--- Lógica/Python/s030_ex4.py
+++ Lógica/Python/s030_ex4.py
@@ -4,5 +4,7 @@ pontuacao = int(input("Digite a pontuação final do jogo: "))
 # Determina o resultado usando o operador ternário
 resultado = "Ganhou" if pontuacao >= 50 else "Perdeu"
 
+# [verdadeiro] if condicao else [falso]
+
 # Imprime o resultado
 print(resultado)
