bbaafeb6bfaff4a0e68bd66088c7a00a9a353ee1
Everton Martins <ecmdigital@hotmail.com>
2025-09-10 16:02:25 -0300

    ...

---
+++ Lógica/Python-Extras/004_dobro.py
@@ -0,0 +1,8 @@
+# Faça um programa que leia um número inteiro e mostre na tela o seu dobro e o seu triplo.
+
+numero = int(input("Digite um número: "))
+
+dobro = numero * 2
+triplo = numero * 3
+
+print(f"O dobro de {numero} é {dobro} e o triplo é {triplo}.")
---
+++ Lógica/Python-Extras/005_notas.py
@@ -0,0 +1,9 @@
+# Desenvolva um programa que peça as duas notas de um aluno, calcule e mostre a sua média final.
+
+# Solução 5
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+media = (nota1 + nota2) / 2
+
+print(f"A média do aluno é: {media}")
